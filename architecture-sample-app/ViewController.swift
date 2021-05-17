b517acafafccc68967408f36b4029d9e11e39a50
riku3 <takarakuten62@gmail.com>
2021-05-17 14:32:52 +0900

    Initial Commit

---
+++ architecture-sample-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  architecture-sample-app
+//
+//  Created by riku on 2021/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
